3ba9d7ae2bebf3a27e0e493b10652e060a80a2dc
collab-rishi <123722983+collab-rishi@users.noreply.github.com>
2025-02-13 13:57:18 +0530

    chore: update books.py

--- api/routes/books.py
+++ api/routes/books.py
@@ -69,6 +69,6 @@ async def get_book_by_id(book_id: int) -> Book:
     if book is None:
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND,
-            content={"detail": "testing this endpoint"}
+            content={"detail": "Book not found"}
         )
     return book
